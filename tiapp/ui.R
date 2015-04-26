8fa2a5e220eea517873a78edd188286cc79279f5
Raju Varghese <raju@intellisoft.ch>
2015-04-26 13:45:13 +0200

    Markdown include files for the 3 tabs

--- tiapp/ui.R
+++ tiapp/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library (markdown)
 
 ti <- read.csv ("./top-income.csv")
 names (ti) <- c("Country", "Year", "Top1000", "Top0100", "Top0010", "Top0001")
@@ -6,14 +7,6 @@ names (ti) <- c("Country", "Year", "Top1000", "Top0100", "Top0010", "Top0001")
 countries <- as.character (unique (ti[,"Country"]))
 yearMin <- min (ti[,"Year"])
 yearMax <- max (ti[,"Year"])
-
-helpPage <- div (
-  p ("The side panel on the left has 3 widgets for setting parameters.",
-     "On changing any of the three parameters the graph and the table in the main panel will be automatically updated"),
-  p ("B"),
-  h2 ("X"),
-  p ("Z")
-  )
 
 # Define UI for application that Top Income Share
 shinyUI(fluidPage(
@@ -24,10 +17,12 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
+      p ("Input controls. For information on how to use them please click on the Help tab on the right"),
+      br (),
       selectInput('countries', 'Countries:', countries, multiple=TRUE, selectize=TRUE, selected=c("Switzerland", "United States")),
       sliderInput("range", "Year:", sep="",
                   min = yearMin, max = yearMax, value = c(1800, 2015)),
-      radioButtons("percentile", "Percentile:",
+      radioButtons("percentile", "Income Percentile:",
                    c("Top 10.0%"  = "Top1000",
                      "Top  1.0%"  = "Top0100",
                      "Top  0.1%"  = "Top0010",
@@ -40,11 +35,13 @@ shinyUI(fluidPage(
         tabPanel ("Graph", h4 ("Graph of Top Income"),
                   plotOutput ("plot")),
         tabPanel ("Table", h4 ("Table of Top Income"),
-                  dataTableOutput ("table"))
-#         tabPanel ("Data Source", h4 ("Information on the Data Used in this Application"),
-#                   helpText ("table"), helpText ("More stuff")),
-#         tabPanel ("Help",  h4 ("Help on using the Application"),
-#                   helpPage)
+                  dataTableOutput ("table")),
+        tabPanel ("Data Source", h4 ("Information on the Data Used in this Application"),
+                  includeMarkdown ("dataSourcePage.md")),
+        tabPanel ("Help",  h4 ("Help on using the Application"),
+                  includeMarkdown ("helpPage.md")),
+        tabPanel ("About",  h4 ("About this Application"),
+                  includeMarkdown ("aboutPage.md"))        
       )
     )
   )
